3ca1adc85fd9b1d9039539de701fb7f994e83607
Sam Smith <samtrey@gmail.com>
2016-09-02 18:26:24 -0500

    final

---
+++ assets/javascript/app.js
@@ -0,0 +1,99 @@
+var shows = [
+    "Deadwood", "The Wire", "Breaking Bad", "Rick and Morty", "Star Trek", "Enlightened", "Game of Thrones", "Peep Show", "The Mighty Boosh", "IT Crowd",
+    "Summer Heights High", "The Sopranos", "Silicon Valley", "Luther", "Sherlock", "Home Movies", "Space Ghost Coast to Coast", "The Venture Brothers",
+    "Halt and Catch Fire", "Mad Men", "Arrested Development", "House of Cards", "The Office", "Orange is the New Black", "True Detective", "Firefly",
+    "Cowboy Bebop", "Seinfeld", "Twlight Zone", "It's Always Sunny In Philadelphia", "Battlestar Galactica", "Spaced", "Berserk", "The West Wing",
+    "Black Mirror", "Garth Merenghi's Dark Place", "Black Books", "Louie", "Baskets", "Mr. Robot", "Band Of Brothers", "Broadchurch", "Limmy's Show",
+    "Party Down",
+];
+
+
+$(document).ready(function() {
+
+
+    function addShow() {
+
+        $('.buttons').empty();
+
+        for (var i = 0; i < shows.length; ++i) {
+
+            var showButtons = $('<button type="button" class="btn-lg btn-primary" value = "" data-toggle="button" aria-pressed="false" autocomplete="off">' + shows[i] + '</button>');
+            showButtons.attr('data-name', shows[i]);
+            showButtons.addClass("MYCLASS");
+            $('.buttons').append(showButtons);
+
+        }
+        $(".MYCLASS").on('click', function() {
+
+            var clickName = $(this).data('name');
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + clickName + "&api_key=dc6zaTOxFJmzC";
+
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+
+            .done(function(response) {
+                console.log(response);
+                var results = response.data;
+
+
+                for (var i = 0; i < 10; i++) {
+
+                    if (results[i].rating == "r" || results[i].rating == "pg-13") {
+
+                    } else {
+
+                        var gifDiv = $('<div class ="items">');
+
+
+                        var gif = $('<img>');
+                        gif.addClass("imgClass");
+                        gif.attr('data-state', 'still');
+                        gif.attr('src', results[i].images.original_still.url);
+                        gif.attr('data-still', results[i].images.original_still.url);
+                        gif.attr('data-animate', results[i].images.original.url);
+
+                        // gifDiv.append(gif);
+
+                        $('.gifsHere').prepend(gif);
+
+                    }
+                }
+
+                $('.imgClass').on('click', function() {
+
+                    var state = $(this).attr('data-state');
+
+                    if (state == 'still') {
+                        $(this).attr('src', $(this).data('animate'));
+                        $(this).attr('data-state', 'animate');
+                    } else {
+                        $(this).attr('src', $(this).data('still'));
+                        $(this).attr('data-state', 'still');
+                    }
+
+                });
+            });
+        });
+    }
+    addShow();
+
+    $('.submit').on('click', function() {
+        var search = $('#showInput').val().trim();
+
+        shows.push(search);
+
+        addShow();
+
+        return false;
+
+    });
+});
+
+//ok so we have a number of buttons on the page. 
+//we also have the ability to add new buttons
+//might be nice to have the ability to delete a buttons after we have added it without refreshing the page!
+//
+//When I click one of the buttons I want it to query the giphy API and display 10 gifs related to the text of the button
+//When I click a new button I want the gifs to clear and the new gifs to appear
